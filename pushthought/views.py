3159381a351ab296184048c1242b3a4c0a921314
Boingoloid <matthew.acalin@gmail.com>
2015-12-09 22:11:57 -0800

    fixing about template by removing the pushthought in the render portion of the views.about

--- pushthought/views.py
+++ pushthought/views.py
@@ -16,7 +16,7 @@ def home(request):
 	return render(request, 'home.html')
 
 def about(request):
-    return render(request,'pushthought/about.html',)
+    return render(request,'about.html',)
 
 
 @login_required
