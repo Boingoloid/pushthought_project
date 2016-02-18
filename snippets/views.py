b11bb4b1609228b8b9aa3f66bece9f401c19cf8e
Boingoloid <matthew.acalin@gmail.com>
2016-02-18 00:52:23 -0800

    a

--- snippets/views.py
+++ snippets/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from django.http import HttpResponseRedirect, HttpResponse
