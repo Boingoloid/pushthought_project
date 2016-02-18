50dd2bd46b3d7be4d7050beb1b2254d1cbea0a73
Boingoloid <matthew.acalin@gmail.com>
2016-02-18 01:14:40 -0800

    l

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,7 +4,7 @@ from snippets import views
 
 urlpatterns = [
     url(r'(?P<pk>[0-9])', views.snippet_detail),
-    url(r'^', views.snippet_list),
+    url(r'', views.snippet_list),
     # url(r'^(?P<pk>[0-9]+)', views.snippet_detail),
 
 ]
