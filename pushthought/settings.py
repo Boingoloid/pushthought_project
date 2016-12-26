acd9ecdd01c5b4062e37c3447511a552e262dd71
Boingoloid <matthew.acalin@gmail.com>
2016-12-25 18:30:29 -0800

    static shiz

--- pushthought/settings.py
+++ pushthought/settings.py
@@ -71,6 +71,7 @@ INSTALLED_APPS = (
     'rest_framework',
     'snippets',
     'prime',
+    'whitenoise',
 )
 
 
@@ -83,7 +84,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
 ROOT_URLCONF = 'pushthought.urls'
