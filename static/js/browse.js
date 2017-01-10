ee1093a7a8463fa7ba0a5e052c13ba02a9552ae9
Boingoloid <matthew.acalin@gmail.com>
2017-01-10 12:22:41 -0800

    Brose butten created with hover over action with css and js

--- static/js/browse.js
+++ static/js/browse.js
@@ -1,5 +1,33 @@
 $(document).ready(function() {
 
+    var acc = $('.accordion');
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].onmouseover = function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+      }
+
+      acc[i].onmouseout = function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + 'px';
+        }
+      }
+
+
+    }
+
+
     // Browse Page
     $('.program-container').on('click','.program-item',function(event) {
        var idText = $(this).attr('id');
@@ -16,15 +44,15 @@ $(document).ready(function() {
       window.location.href="http://127.0.0.1:8000/browse";
     });
 
-    $('.segment-item-container').on('click','.segment-item',function(event) {
-       var programObjectId = $("#program-objectId").text();
-
-       var idText = $(this).attr('id');
-       var segmentIndex = idText.replace('segment-item','');
-
-       var segmentObjectIdTagName = "#segment-item-objectId" + segmentIndex;
-       var segmentObjectId = $(segmentObjectIdTagName).text();
-
-       window.location.href="http://127.0.0.1:8000/action_menu/" + programObjectId + "/" + segmentObjectId;
-    });
+//    $('.segment-item-container').on('click','.segment-item',function(event) {
+//       var programObjectId = $("#program-objectId").text();
+//
+//       var idText = $(this).attr('id');
+//       var segmentIndex = idText.replace('segment-item','');
+//
+//       var segmentObjectIdTagName = "#segment-item-objectId" + segmentIndex;
+//       var segmentObjectId = $(segmentObjectIdTagName).text();
+//
+//       window.location.href="http://127.0.0.1:8000/action_menu/" + programObjectId + "/" + segmentObjectId;
+//    });
 });
