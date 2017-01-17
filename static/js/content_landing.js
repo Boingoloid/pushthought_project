aa3e0da3c0271779882f8a78a66bba45463412c9
Boingoloid <matthew.acalin@gmail.com>
2017-01-17 12:06:56 -0800

    workingo on action container

--- static/js/content_landing.js
+++ static/js/content_landing.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-
-
     $('.watch-button').click( function() {
 //       var idText = $(this).attr('id');
 //       var repIndex = idText.replace('program-item','');
@@ -12,5 +10,26 @@ $(document).ready(function() {
        window.location.href="/leaving";
     });
 
+    $('.twitter-icon').click( function() {
+        $('.rep-container').css('height','700px');
+        alert("twitter clicked");
+    });
+
+    $('.phone-icon').click( function() {
+        alert("phone clicked");
+    });
+
+    $('.email-icon').click( function() {
+        alert("email clicked");
+    });
+
+    $('.tweet-container').on('click','.tweet-item',function(event) {
+       var idText = $(this).attr('id');
+       var tweetIndex = idText.replace('tweet-item','');
+
+       var tweetObjectIdElementName = "#tweet-item-objectId" + tweetIndex;
+       var tweetObjectId = $(programObjectIdElementName).text();
+       window.location.href="/content_landing/" + tweetObjectId;
+    });
 
 });
