6ed4cb99a4e182a0d3eaa20117467778c3d70390
Matthew Acalin <matthewacalin@Matthews-MacBook-Pro.local>
2017-02-27 18:25:45 -0800

    ran static

---
+++ staticfiles/js/browse.js
@@ -0,0 +1,122 @@
+function csrfSafeMethod(method) {
+// these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+$(document).ready(function() {
+
+
+
+    $('.browse-button-a').hover( function () {
+        console.log("hello");
+        var acc = $('.accordion');
+        if ($('.panel').css('opacity') == 0){
+            $(".panel").css("opacity", "1.0");
+            $(".panel").css("height", "auto");
+            $(".panel").css("border-top", "3px solid white");
+            console.log("make appear");
+        } else {
+            $(".panel").css("opacity", "0.0");
+            $(".panel").css("height", "0");
+            $(".panel").css("border-top", "0px solid white");
+            console.log("make disappear");
+        }
+    });
+
+
+    $('.program-container').on('click','.program-item',function(event) {
+        function showLoading(index){
+            var loaderDiv = '.loader-' + index;
+            console.log(loaderDiv);
+            $(loaderDiv).show();
+        }
+
+        var idText = $(this).attr('id');
+        var repIndex = idText.replace('program-item','');
+
+        showLoading(repIndex);
+
+
+
+        var programObjectIdElementName = "#program-item-objectId" + repIndex;
+        var programId = $(programObjectIdElementName).text();
+        // alert (programObjectId)
+        window.location.href="/content_landing/" + programId;
+
+//        var csrftoken = Cookies.get('csrftoken');
+//        //var csrftoken = jQuery("[name=csrfmiddlewaretoken]").val();  //this also works
+//        $.ajaxSetup({
+//            beforeSend: function(xhr, settings) {
+//                if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+//                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+//                }
+//            }
+//        });
+
+//
+//       dataSet = JSON.stringify({
+//                "programId": programId
+//        });
+//         $.ajax({url: "/content_landing/" + programId,
+//                type: "GET",
+////                data: dataSet,
+////                contentType: 'application/json;charset=UTF-8',
+////                cache: false,
+//                success: function(data) {
+//                    // Success message
+////                    programId = data['programId']
+////                    window.location.href='/content_landing/' + programId
+//                },
+//                error: function() {
+//                    // Fail message
+//                    console.log('fail :)')
+//                },
+//         });
+
+
+
+    });
+
+
+});
+
+
+
+//    $('.segment-item-container').on('click','.segment-item',function(event) {
+//       var programObjectId = $("#program-objectId").text();
+//
+//       var idText = $(this).attr('id');
+//       var segmentIndex = idText.replace('segment-item','');
+//
+//       var segmentObjectIdTagName = "#segment-item-objectId" + segmentIndex;
+//       var segmentObjectId = $(segmentObjectIdTagName).text();
+//
+//       window.location.href="http://127.0.0.1:8000/action_menu/" + programObjectId + "/" + segmentObjectId;
+//    });
+
+
+
+//    var acc = $('.accordion');
+//    var i;
+//
+//    for (i = 0; i < acc.length; i++) {
+//      acc[i].onmouseover = function() {
+//        this.classList.toggle("active");
+//        var panel = this.nextElementSibling;
+//        if (panel.style.maxHeight){
+//          panel.style.maxHeight = null;
+//        } else {
+//          panel.style.maxHeight = panel.scrollHeight + 'px';
+//        }
+//      }
+//
+//      acc[i].onmouseout = function() {
+//        this.classList.toggle("active");
+//        var panel = this.nextElementSibling;
+//        if (panel.style.maxHeight){
+//          panel.style.maxHeight = null;
+//        } else {
+//          panel.style.maxHeight = panel.scrollHeight + 'px';
+//        }
+//      }
+//    }
---
+++ staticfiles/js/user_signin_form.js
@@ -0,0 +1,119 @@
+$(document).ready(function() {
+    window.setTimeout(function() {
+        $(".alert").fadeTo(3000, 0)
+    }, 3000);
+
+     $('#login-btn').on('click',function() {
+        window.location.href="http://127.0.0.1:8000/login_form";
+     });
+
+
+    //function validationFunc(){
+        $('#create-form').validate({ // initialize the plugin
+            rules: {
+                user_email: {
+                    required: true,
+                    email: true
+                },
+                password: {
+                    required: true,
+                    minlength: 5
+                },
+                confirm_password: {
+                    required: true,
+                    minlength: 5,
+                    equalTo: "#password"
+                }
+            },
+            messages: {
+                user_email: {
+                    required: "Please enter a valid email"
+                },
+                password: {
+                    required: "Please enter a password",
+                    minlength: "must be at least 5 characters long"
+                },
+                confirm_password: {
+                    required: "Please confirm password",
+                    minlength: "must be at least 5 characters long",
+                    equalTo: "Passwords must match"
+                }
+            },
+            submitHandler: function(form) {
+                var csrftoken = Cookies.get('csrftoken');
+
+                function csrfSafeMethod(method) {
+                    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+                }
+
+                $.ajaxSetup({
+                    beforeSend: function(xhr, settings) {
+                        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                        }
+                    }
+                });
+
+                $.post("http://127.0.0.1:8000/user_signin_form/", $("#create-form").serialize());
+                 console.log("Do some stuff...");
+                 //submit via ajax
+                  return false;  //This doesn't prevent the form from submitting.
+            }
+        });
+
+
+//        var csrftoken = Cookies.get('csrftoken');
+//        alert("CSRF token:" + csrftoken);
+//
+//        function csrfSafeMethod(method) {
+//        // these HTTP methods do not require CSRF protection
+//            return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+//        }
+//        $.ajaxSetup({
+//            beforeSend: function(xhr, settings) {
+//                if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+//                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+//                }
+//            }
+//        });
+//
+//        $.post("http://www.pushthought.com/action_menu/verify_twitter/",
+//          {
+//            tweetText: tweetText,
+//            actionCategory: "Local Representative",
+//            messageCategory: "Local Representative",
+//            segmentId: segmentId,
+//            programId: programId
+//          }
+//            );
+
+
+
+//    $('#create-account-btn').click(function() {
+//        email = $('#email').val();
+//        password = $('#password').val();
+//        confirm_password = $('#confirm_password').val();
+
+//        if (email.length ==0 || password.length ==0 || confirm_password.length ==0){
+//            alert("please fill out all the fields");
+//        } else {
+//            console.log("length");
+//            console.log(email.length);
+
+//            validationFunc();
+
+});
+
+//var password = document.getElementById("password")
+//  , confirm_password = document.getElementById("confirm_password");
+//
+//function validatePassword() {
+//  if (password.value != confirm_password.value) {
+//    confirm_password.setCustomValidity("Passwords Don't Match");
+//  } else {
+//    confirm_password.setCustomValidity('');
+//  }
+//}
+//
+//password.onchange = validatePassword;
+//confirm_password.onkeyup = validatePassword;
