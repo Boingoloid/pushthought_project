cd11acc0bfa3dc4ee29121fd8d6c0e9ae05cc050
Viacheslav Khromiak <slava.khromyak@gmail.com>
2017-04-14 04:27:45 +0300

    update static files settings

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -1,6 +1,18 @@
 from .base import *
 import dj_database_url
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 DATABASES['default'] =  dj_database_url.config()
