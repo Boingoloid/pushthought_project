79598519cb0efd88a4682b2bdfe794a278358ce8
Viacheslav Khromiak <slava.khromyak@gmail.com>
2017-04-14 04:02:51 +0300

    update admin

--- programs/admin.py
+++ programs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Program)
+admin.site.register(models.Season)
+admin.site.register(models.Episode)
+admin.site.register(models.Segment)
