3a329f017bfe9b5b77b06bbcd5b0e95e0c5d5743
Viacheslav Khromiak <slava.khromyak@gmail.com>
2017-04-15 00:02:58 +0300

    add string to admin

--- programs/models.py
+++ programs/models.py
@@ -12,18 +12,30 @@ class Program(TimeStampedModel):
     runtime = models.IntegerField()
     type = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.title
+
 
 class Season(TimeStampedModel):
     program = models.ForeignKey('Program')
     number = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return '{} season {}'.format(self.program, self.number)
 
 
 class Episode(TimeStampedModel):
     season = models.ForeignKey('Season')
     number = models.PositiveSmallIntegerField()
 
+    def __str__(self):
+        return ' {} episode {}'.format(self.season, self.number)
+
 
 class Segment(models.Model):
     program = models.ForeignKey('Program')
     episode = models.ForeignKey('Episode', blank=True, null=True)
     duration = models.IntegerField()
+
+    def __str__(self):
+        return '{}'.format(self.program)
