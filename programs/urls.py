58d0bb0b797b92b67d9c6fde9b169fb87a56b0be
Viacheslav Khromiak <slava.khromyak@gmail.com>
2017-04-19 04:51:07 +0300

    add search by title

--- programs/urls.py
+++ programs/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^(?P<pk>\d+)$', views.ProgramDetailView.as_view(), name='detail'),
-    url(r'^search/imdb/id/$', views.SearchIMDBProgramIDView.as_view(), name='search_imdb_id'),
+    url(r'^add/imdb/id/$', views.SearchIMDBProgramIDView.as_view(), name='add_imdb_id'),
+    url(r'^search/imdb/name/$', views.SearchIMDBProgramTitleView.as_view(), name='search_imdb_title'),
 ]
