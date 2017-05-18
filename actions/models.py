c8bb328fdb69f3de7aa7f00207b168a97ada0809
jtikey <Y03qy013>
2017-05-18 03:25:59 +0300

    Add hashtags

--- actions/models.py
+++ actions/models.py
@@ -6,15 +6,18 @@ from django_extensions.db.models import TimeStampedModel
 from django.contrib.auth.models import User
 
 from congress.models import CongressCounter
-
+from hashtag.models import Hashtag
 
 class Tweet(TimeStampedModel):
     text = models.TextField(max_length=140)
     action = models.OneToOneField('Action')
 
+    def save(self, **kwargs):
+
+        super(Tweet, self).save(**kwargs)
+
 
 class SaveTweetManager(models.Manager):
-
     def create(self, text, *args, **kwargs):
         action = super(SaveTweetManager, self).create(**kwargs)
         tweet, saved = Tweet.objects.get_or_create(
@@ -22,25 +25,22 @@ class SaveTweetManager(models.Manager):
             action=action,
         )
         if saved:
+            Hashtag.hashtags.parse_mentions(text, action.program)
             return tweet
 
 
 class Action(TimeStampedModel):
     user = models.ForeignKey(User)
-    program = models.ForeignKey('programs.Program', related_name='counter_model')
+    program = models.ForeignKey('programs.Program', related_name='actions')
     congress = models.ForeignKey('congress.Congress', blank=True, null=True)
     tweets = SaveTweetManager()
 
     def save(self, **kwargs):
-        increase_action = False
         if not self.id:
-            increase_action = True
-        super(Action, self).save(**kwargs)
-        if increase_action:
             counter, created = CongressCounter.objects.get_or_create(
                 program=self.program,
                 congress=self.congress,
             )
             counter.increase()
             self.program.increase()
-
+        super(Action, self).save(**kwargs)
--- config/settings/base.py
+++ config/settings/base.py
@@ -50,6 +50,7 @@ LOCAL_APPS = [
     'congress',
     'users',
     'actions',
+    'hashtag',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
