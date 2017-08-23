d20db31f20dfed4029bc25d08d164fa9bd8772b7
jtikey <Y03qy013>
2017-08-24 02:21:26 +0300

    add url validation

--- static/js/campaign_form.js
+++ static/js/campaign_form.js
@@ -1,38 +1,39 @@
 $(document).ready(function() {
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        checkUrl();
+        if (slug_result === 'Taken') {
+            alert('url taken!');
+            return false
+        }
 
+        var url = $('input[name=link]').val();
+        url = validateUrl(url);
+        if (!url) {
+            alert("url error");
+            return false
+        }
 
-//    jQuery(function ($) {
-//        var $textareas = $('textarea[name=field-input-tweet-text],textarea[name=field-input-email-text]');
-//        $textareas.on('textarea', function () {
-//            // Set the required property of the other input to false if this input is not empty.
-//            $textareas.not(this).prop('required', !$(this).val().length);
-//        });
-//    });
+        // Perform validation
+        var error = false;
 
+        if($('textarea[name=tweet_text]').val() || $('textarea[name=email_text]').val()) {
+            alert('Passed validation');
+            error = false;
+        } else {
+            alert('Please fill up one field');
+            error = true;
+        }
 
-        $('form').on('submit', function(e) {
-            e.preventDefault();
-            checkUrl();
-            if (slug_result === 'Taken') {
-                alert('url taken!');
-                return false
-            }
-
-            // Perform validation
-            var error = false;
-
-            if($('textarea[name=tweet_text]').val() || $('textarea[name=email_text]').val()) {
-                alert('Passed validation');
-                error = false;
-            } else {
-                alert('Please fill up one field');
-                error = true;
-            }
-
-            // Check error flag before submission
-            if(!error) $(this)[0].submit();
-        });
-
-
-
+        // Check error flag before submission
+        if(!error) $(this)[0].submit();
+    });
 });
+
+function validateUrl(url) {
+    var re = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
+    if (!re.test(url)) {
+        return false;
+    }
+    return true
+}
--- static/js/check_campaign.js
+++ static/js/check_campaign.js
@@ -6,9 +6,11 @@ $('#slug').on('change', function (e) {
 
 function checkUrl() {
     var slug = $('#slug').val();
+    var slugged = slugify(slug);
+    $('#slug').val(slugged);
     $.ajax({
         async: false,
-        url: '/campaign/check/' + '?slug=' + slug,
+        url: '/campaign/check/' + '?slug=' + slugged,
         success: function (data) {
             if (data.result) {
                 slug_result = 'Taken'
@@ -18,4 +20,14 @@ function checkUrl() {
             $('#check_result').text(slug_result)
         }
     })
+}
+
+function slugify(text)
+{
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
 }
