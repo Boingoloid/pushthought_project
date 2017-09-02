c4a495fd84ba012ec4763b833c3c6cee93e78ddd
ben <zorrotmm@hotmail.com>
2017-09-02 13:53:47 -0300

    Sharing functionality added to 3 locations.  URL slug editing disabled for campaigns.

--- static/js/content_landing.js
+++ static/js/content_landing.js
@@ -4,6 +4,22 @@ $(document).ready(function() {
 
     var windowURL = window.location.href;
 
+    // Facebook and Twitter sharing buttons
+    var current_url = window.location.href;
+    var fb_text = $("p.description").text().length ? $("p.description").text() : 'I just contacted my congressional reps on Push Thought'
+    params = {
+      'text': 'I just contacted my congressional reps on Push Thought',
+      'url': current_url
+    }
+    $("#twitter-share-button").prop('href', "https://twitter.com/intent/tweet?"+$.param(params));
+
+    $("#facebook-share-button").click(function(){
+      FB.ui({
+        method: 'share',
+        href: current_url,
+        quote: fb_text,
+      }, function(response){});
+    });
 
         // Zip submission button click
     $('.submit-zip').click( function() {
