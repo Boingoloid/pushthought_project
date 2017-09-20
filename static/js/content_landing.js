e3abb276c97e2bf138056258afe59c235d4ad64f
matthewacalin <matthew.acalin@gmail.com>
2017-09-19 21:32:55 -0700

    entered space outside of addressplaceholder

--- static/js/content_landing.js
+++ static/js/content_landing.js
@@ -330,7 +330,7 @@ $(document).ready(function() {
         addressPlaceholder = String(addressPlaceholder);
 
         console.log("address placeholder: ", addressPlaceholder);
-        $('#text-input').html('<span contenteditable=false class="address-placeholder">Congressperson '+  addressPlaceholder +',</span>');
+        $('#text-input').html('<span contenteditable=false class="address-placeholder">Congressperson '+  addressPlaceholder +', </span>');
         console.log($('#text-input').html());
 
 
@@ -364,9 +364,8 @@ $(document).ready(function() {
         var labelText = 'email: ' + countSelected;
         $('#email-button-label').text(labelText);
 
-        // Focus on text box
-        $('#text-input').focus();
-        setEndOfContenteditable($('#text-input'));
+
+        //setEndOfContenteditable($('#text-input'));
         updateTextCount();
 
 
@@ -403,7 +402,8 @@ $(document).ready(function() {
 //        }
         //<div hidden id="emailFields" data-emailFields="{{ currentUser.congressEmailFields }}"></div>
     });
-
+        // Focus on text box
+        $('#text-input').focus();
 
     // Twitter Icon
     $('.rep-container').on("click", "img.twitter-icon", function(e) {
@@ -473,8 +473,8 @@ $(document).ready(function() {
         // insert address placeholder in text-input
         var addressPlaceholderClass = '.address-item-label-' + i;
         var addressPlaceholder = $(addressPlaceholderClass).text();
-        addressPlaceholder = String(addressPlaceholder + ' '); // Space important! allows @ to be recognized
-        $('#text-input').html('<span contenteditable=false class="address-placeholder">'+addressPlaceholder+'</span>');
+        addressPlaceholder = String(addressPlaceholder); // Space important! allows @ to be recognized
+        $('#text-input').html('<span contenteditable=false class="address-placeholder">'+addressPlaceholder+' </span>');
 
         // set button label
         var numItems = $('.address-item.selected').length;
@@ -487,9 +487,10 @@ $(document).ready(function() {
         } else {
             //console.log("focus, width high");
             $('#text-input').focus();
-            setEndOfContenteditable($('#text-input'));
+            //setEndOfContenteditable($('#text-input'));
         }
         updateTextCount();
+        $('#text-input').focus();
     });
 
     // Empty Twitter Icon
@@ -1007,23 +1008,23 @@ $(document).ready(function() {
 
 
 
-function setEndOfContenteditable(contentEditableElement)
-{
-    var range,selection;
-    if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
-    {
-        range = document.createRange();//Create a range (a range is a like the selection but invisible)
-        range.selectNodeContents(contentEditableElement[0]);//Select the entire contents of the element with the range
-        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
-        selection = window.getSelection();//get the selection object (allows you to change selection)
-        selection.removeAllRanges();//remove any selections already made
-        selection.addRange(range);//make the range you have just created the visible selection
-    }
-    else if(document.selection)//IE 8 and lower
-    {
-        range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
-        range.moveToElementText(contentEditableElement[0]);//Select the entire contents of the element with the range
-        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
-        range.select();//Select the range (make it the visible selection
-    }
-}
+//function setEndOfContenteditable(contentEditableElement)
+//{
+//    var range,selection;
+//    if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+//    {
+//        range = document.createRange();//Create a range (a range is a like the selection but invisible)
+//        range.selectNodeContents(contentEditableElement[0]);//Select the entire contents of the element with the range
+//        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+//        selection = window.getSelection();//get the selection object (allows you to change selection)
+//        selection.removeAllRanges();//remove any selections already made
+//        selection.addRange(range);//make the range you have just created the visible selection
+//    }
+//    else if(document.selection)//IE 8 and lower
+//    {
+//        range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+//        range.moveToElementText(contentEditableElement[0]);//Select the entire contents of the element with the range
+//        range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+//        range.select();//Select the range (make it the visible selection
+//    }
+//}
