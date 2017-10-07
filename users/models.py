ad555519d5c6d53418a4f82944e65fd7e07e64a6
jtikey <Y03qy013>
2017-10-08 01:54:20 +0300

    add twitter name

--- users/models.py
+++ users/models.py
@@ -8,6 +8,8 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
+from allauth.socialaccount.models import SocialAccount
+
 
 # Create your models here.
 class Profile(TimeStampedModel):
@@ -18,6 +20,12 @@ class Profile(TimeStampedModel):
     city = models.CharField(max_length=50, blank=True, null=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
     zip = models.CharField(max_length=10, blank=True, null=True)
+
+    @property
+    def twitter(self):
+        token = SocialAccount.objects.filter(user=self.user, provider='twitter').first()
+        if token:
+            return token.extra_data['screen_name']
 
     def __str__(self):
         return '{}'.format(self.user)
