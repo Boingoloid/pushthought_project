0328d7a2808e71c4ac45d2e67c529ae216e1ecfc
jtikey <Y03qy013>
2017-11-12 01:27:35 +0200

    fix parser validator

--- static/js/browse/imdb_search.js
+++ static/js/browse/imdb_search.js
@@ -1,14 +1,4 @@
 $(document).ready(function () {
-    // $('#search_keyword').on('keyup', function () {
-    //     let q = $(this).val();
-    //     $.ajax({
-    //         url: imdb_id_search_url + '?q=' + q,
-    //         success: function (data) {
-    //
-    //         }
-    //     })
-    // })
-
     $('#search_btn').on('click', function () {
         var text_in_search = String($('#search_keyword').val());
 
@@ -21,30 +11,7 @@ $(document).ready(function () {
         }
 
 
-        var validation = false;
-
-        var youtube_https = text_in_search.substring(0, 23);
-        var youtube_www = text_in_search.substring(0, 15);
-        console.log('youtube_https: '+ youtube_https);
-        console.log("youtube_www: " + youtube_www);
-
-        if(youtube_https == "https://www.youtube.com" || youtube_www == "www.youtube.com"){
-            validation = true;
-            console.log("youtube validated");
-        }
-
-        var imdb_http = text_in_search.substring(0, 19);
-        var imdb_www = text_in_search.substring(0, 12);
-
-        console.log("imdb_http: " + imdb_http);
-        console.log("imdb_www: " + imdb_www);
-
-        if(imdb_http == "http://www.imdb.com" ||   imdb_www == "www.imdb.com"){
-
-            validation = true;
-            console.log("imdb validated");
-
-        }
+        var validation = text_in_search.match(/(youtube.com|imdb.com)/);
 
         if(validation){
             let imdb_url = $('#search_keyword').val();
